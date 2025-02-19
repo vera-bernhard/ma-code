7165368bd0fb5a3f6962c6d033dea03c30cc7f82
vera-bernhard <vera.l.bernhard@bluewin.ch>
2025-02-19 18:47:13 +0100

    Update finetune

--- finetune_whisper/finetune.py
+++ finetune_whisper/finetune.py
@@ -3,7 +3,7 @@ import random
 import torch
 import pandas as pd
 import wandb
-from datasets import DatasetDict, Audio, Dataset
+from datasets import DatasetDict, Audio, Dataset, IterableDataset
 from transformers import (
     WhisperForConditionalGeneration,
     WhisperProcessor,
@@ -14,7 +14,7 @@ from transformers import (
 )
 from jiwer import wer
 from dataclasses import dataclass
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Union, Generator
 from tqdm import tqdm
 import csv
 import logging
@@ -32,10 +32,18 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
         logging.FileHandler("log/preprocess.out"),
-        logging.StreamHandler(sys.stdout)
     ],
-    force=True)
+    force=True
+)
+
+# logger for console
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.WARNING)
+console_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s'))
+
 logger = logging.getLogger()
+logger.addHandler(console_handler)
 
 
 @dataclass
@@ -151,12 +159,20 @@ def preprocess(dataset: DatasetDict, whisper_size: str = 'small', outdir: str = 
 
     os.makedirs(outdir, exist_ok=True)
 
-    def preprocess_and_save(batch: dict[dict], idx: int, split_name: str) -> tuple[Dict, str]:
+    def preprocess_and_save(batch: dict, idx: int, split_name: str) -> str:
+        """Processes a single batch and saves to disk immediately."""
+        file_path = os.path.join(split_outdir, f"data_{idx}.pt")
+
+        if os.path.exists(file_path):
+            logger.info(f"File {file_path} already exists. Skipping...")
+            return file_path  # Only return the file path
+
         audio_array = batch['audio']['array']
         if audio_array is None or audio_array.size == 0:
             logger.warning(f"Faulty audio: {batch['audio']['path']}")
             return None
 
+        # Process audio
         audio_input = feature_extractor(
             audio_array, sampling_rate=batch['audio']['sampling_rate'], return_tensors="pt")
         input_features = audio_input["input_features"]
@@ -164,6 +180,7 @@ def preprocess(dataset: DatasetDict, whisper_size: str = 'small', outdir: str = 
             0, 3000 - input_features.shape[-1])), mode='constant', value=0)
         input_features = input_features[:, :3000]
 
+        # Process text
         token_data = tokenizer(batch["text"], padding="max_length",
                                truncation=True, max_length=448, return_tensors="pt")
 
@@ -173,47 +190,60 @@ def preprocess(dataset: DatasetDict, whisper_size: str = 'small', outdir: str = 
             "attention_mask": token_data.attention_mask[0],
             "audio_path": batch["audio_path"]
         }
-        file_path = os.path.join(outdir, f"{split_name}_data_{idx}.pt")
-        torch.save(processed_data, file_path)
-        return processed_data, file_path
 
-    intermediate_files = []
-    processed_data_dict = {}
+        torch.save(processed_data, file_path)
+        return file_path
 
     for split_name, split_dataset in dataset.items():
         logger.info(f"Processing split: {split_name}")
-        processed_samples = []
+
+        split_outdir = os.path.join(outdir, split_name)
+        os.makedirs(split_outdir, exist_ok=True)
 
         for idx, example in enumerate(tqdm(split_dataset, desc=f"Preprocessing {split_name}")):
             try:
-                data, file = preprocess_and_save(example, idx, split_name)
-                if data:
-                    processed_samples.append(data)
-                    intermediate_files.append(file)
+                preprocess_and_save(example, idx, split_outdir)
             except Exception as e:
                 logger.error(
                     f"Failed to process {split_name} index {idx}: {e}")
 
-        processed_data_dict[split_name] = processed_samples
-
-    if processed_data_dict:
-        final_dataset_dict = DatasetDict({
-            split_name: Dataset.from_list(processed_samples)
-            for split_name, processed_samples in processed_data_dict.items()
-        })
-        final_dataset_dict.save_to_disk(outdir)
-        logger.info(f"Preprocessed dataset saved to {outdir}")
-
-    # Delete intermediate files
-    for file in intermediate_files:
-        os.remove(file)
-
-    logger.info('All splits processed and saved successfully.')
+    logger.info(f"Preprocessed dataset saved to {outdir}")
     return outdir
 
 
-def fine_tune(feat_dir: str, whisper_size: str = 'small', save_path: str = "./finetune_whisper"):
-    dataset = DatasetDict.load_from_disk(feat_dir)
+def load_data_generator(split_dir: str, split: str, subset: float = 1.0) -> Generator:
+    file_paths = sorted(os.listdir(os.path.join(split_dir, split)))
+    if subset < 1:
+        file_paths = file_paths[:int(subset * len(file_paths))]
+    for file_name in file_paths:
+        file_path = os.path.join(split_dir, split, file_name)
+        yield torch.load(file_path, weights_only=True)
+
+
+def load_data_split(split_dir: str, split: str, subset: float = 1.0) -> tuple[IterableDataset, int]:
+    files = os.listdir(os.path.join(split_dir, split))
+    if subset < 1:
+        files = files[:int(subset * len(files))]
+    return IterableDataset.from_generator(
+        lambda: load_data_generator(split_dir, split, subset)), len(files)
+
+
+def load_data_splits(feat_dir: str, subset: float = 1.0):
+    return DatasetDict({
+        split_name: IterableDataset.from_generator(
+            lambda split_dir=split_name: load_data_generator(feat_dir, split_dir, subset))
+        for split_name in os.listdir(feat_dir)
+    })
+
+
+def fine_tune(feat_dir: str, whisper_size: str = 'small', save_path: str = "./finetune_whisper", batch_size: int = 4):
+    dataset = load_data_splits(feat_dir)
+
+    # TODO: some redundancy as listdir is called in load_data_splits already
+    num_train_samples = len(os.listdir(os.path.join(feat_dir, "train")))
+    max_steps = num_train_samples // batch_size
+
+    # dataset = DatasetDict.load_from_disk(feat_dir)
     model = WhisperForConditionalGeneration.from_pretrained(
         f"openai/whisper-{whisper_size}")
 
@@ -231,14 +261,15 @@ def fine_tune(feat_dir: str, whisper_size: str = 'small', save_path: str = "./fi
         output_dir="./finetune_whisper",
         eval_strategy="steps",
         eval_steps=500,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         save_strategy="steps",
         save_steps=500,
         save_total_limit=1,
         logging_dir="./logs",
         learning_rate=1e-5,
         warmup_steps=500,
+        max_steps=max_steps,
         num_train_epochs=3,
         weight_decay=0.01,
         fp16=True,
@@ -264,7 +295,14 @@ def fine_tune(feat_dir: str, whisper_size: str = 'small', save_path: str = "./fi
     predict(trainer, dataset["test"], "predictions.csv")
 
 
-def predict(trainer: Trainer, dataset: Dataset, outfile: str, model_path: str = None, whisper_size: str = 'small'):
+def predict(trainer: Trainer, dataset: Union[Dataset, IterableDataset], outfile: str, model_path: str = None, whisper_size: str = 'small', data_size: int = None):
+
+    # Note: iterable datasets require the data_size to be provided, as they do not have a length attribute
+    if type(dataset) == IterableDataset:
+        if data_size is None:
+            raise ValueError(
+                "If dataset is of type IterableDataset, you must provide the data_size")
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Case 1: Fine-tuned model
@@ -288,26 +326,27 @@ def predict(trainer: Trainer, dataset: Dataset, outfile: str, model_path: str = 
     processor = WhisperProcessor.from_pretrained(
         f"openai/whisper-{whisper_size}")
     model.eval()
-
     with open(outfile, mode='a', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
 
+        # Wrtie header if file is empty
         if f.tell() == 0:
             writer.writerow(["audio_file", "true_text", "pred_text"])
 
         with torch.no_grad():
-            for i in tqdm(range(len(dataset)), desc="Predicting"):
-                batch = dataset[i]
-                input_features = torch.tensor(
-                    batch["input_features"]).unsqueeze(0).to(device)
+            dataset_size = data_size if data_size else len(dataset)
+            for i, batch in tqdm(enumerate(dataset), total=dataset_size, desc="Predicting"):
+                input_features = batch["input_features"].clone(
+                ).detach().unsqueeze(0).to(device)
+
                 attention_mask = batch.get("attention_mask", None)
 
                 generated_ids = model.generate(
                     input_features, attention_mask=attention_mask)
                 pred_text = processor.batch_decode(
                     generated_ids, skip_special_tokens=True)[0]
-                true_text = processor.batch_decode(torch.tensor(
-                    batch["labels"]).unsqueeze(0), skip_special_tokens=True)[0]
+                true_text = processor.batch_decode(
+                    batch["labels"].clone().detach().unsqueeze(0), skip_special_tokens=True)[0]
                 audio_file = batch.get("audio_path", "Unknown")
 
                 writer.writerow([audio_file, true_text, pred_text])
@@ -336,21 +375,21 @@ if __name__ == "__main__":
     output_path = "/home/vebern/data/ma-code/finetune_whisper/finetune_whisper_small"
     output_feat_path = "/home/vebern/scratch/srf_ad_feat"
 
-
-    train_file, val_file, test_file = make_split(
-        data_path, output_path)
     model_size = 'small'
     sizes = ['small', 'medium', 'large']
 
-    dataset = load_custom_dataset(train_file, val_file, test_file)
-    output_feat_path = preprocess(dataset, model_size, output_feat_path)
+    # train_file, val_file, test_file = make_split(
+    #     data_path, output_path)
 
-    # load preprocessed dataset
-    dataset = DatasetDict.load_from_disk(output_feat_path)
+    # dataset = load_custom_dataset(train_file, val_file, test_file)
+    # output_feat_path = preprocess(dataset, model_size, output_feat_path)
 
-    # let not fine-tuned model predict on test set
-    predict(None, dataset["test"], f"predictions_whisper_{model_size}_untrained.csv",
-            model_path=None, whisper_size=model_size)
+    # load preprocessed datasez
+
+    # # let not fine-tuned model predict on test set
+    # test_set, nr_test_set = load_data_split(output_feat_path, "test", 0.01)
+    # predict(None, test_set, f"predictions_whisper_{model_size}_untrained.csv",
+    # model_path = None, whisper_size = model_size, data_size = nr_test_set)
 
     fine_tune(output_feat_path, model_size,
               save_path=f"./fine_tuned_whisper_{model_size}")
